19c0cc1ed5714683933a38103fa26663e33591ee
BalikaRahul <rr7511489@gmail.com>
2025-12-02 12:36:12 +0530

    adding files

---
+++ python.py
@@ -0,0 +1,20 @@
+def reverse(a,r):
+    # if(len(str(a))==0):
+    #     return[]
+    # if(len(str(a))==1):
+    #     return a
+    for i in range(len(str(a))):
+        divisor=a//10
+        divisor=remainder
+        r=r*10+divisor
+        return r
+
+        
+
+
+a=4543
+r=""
+divisor=0
+remainder=0
+print(reverse(a,r))
+
