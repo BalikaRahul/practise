c87e8a69741bcad974a700dc1062bf4fdb158416
BalikaRahul <rr7511489@gmail.com>
2025-12-05 15:00:41 +0530

    day 4

--- python.py
+++ python.py
@@ -1,12 +1,31 @@
-def reverse(a,r):
-    for i in range(len(str(a))):
-        r=a[::-1]
-        if (r==a):
-            return "it is a"
-        else:
-            return "it is not a"
-    reverse(a,r)
+def sort(a):
+    if(len(a)>1):
+        mid=len(a)//2
+        left =a[:mid]
+        right=a[mid:]
+        sort(left)
+        sort(right)
+        i=j=k=0
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                a[k]=left[i]
+                i+=1
+            else:
+                a[k]=right[j]
+                j+=1
+            k+=1
+        
+        while i<len(left):
+            a[k]=left[i]
+            i+=1
+            k+=1
+        while j<len(right):
+            a[k]=right[j]
+            j+=1
+            k+=1
 
+    return a
 
-a="racecar"
-r=""
+a=[1,2,4,3,6,5,8,0]
+result = sort(a)
+print(result)
