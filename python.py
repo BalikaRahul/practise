50ed3cbbded76c285052dbca9226542fa7c61b3f
BalikaRahul <rr7511489@gmail.com>
2025-12-06 21:15:03 +0530

    day5

--- python.py
+++ python.py
@@ -1,31 +1,15 @@
-def sort(a):
-    if(len(a)>1):
-        mid=len(a)//2
-        left =a[:mid]
-        right=a[mid:]
-        sort(left)
-        sort(right)
-        i=j=k=0
-        while i<len(left) and j<len(right):
-            if left[i]<right[j]:
-                a[k]=left[i]
-                i+=1
-            else:
-                a[k]=right[j]
-                j+=1
-            k+=1
-        
-        while i<len(left):
-            a[k]=left[i]
-            i+=1
-            k+=1
-        while j<len(right):
-            a[k]=right[j]
-            j+=1
-            k+=1
+def sort (a,i=0):
+    n =len(a)
+    if i==n:
+        return 
+    key =a[i]
+    j=i-1
+    while(j>=0 and a[j]>key):
+        a[j+1]= a[j]
+        j-=1
+    a[j+1]= key
 
-    return a
-
-a=[1,2,4,3,6,5,8,0]
+    return sort (a,i+1)
+a=[1,4,3,2,5]
 result = sort(a)
-print(result)
+print(a)
